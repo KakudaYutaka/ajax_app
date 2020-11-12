45b3001574f895d0ac09cf1bfafc5e85eb32d9a7
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-12 19:19:31 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,9 @@ class PostsController < ApplicationController
   end
 
 
-  def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+  def create ##メモ作成時に未読の情報を保存 レスポンスをJSON
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
 
   def checked
